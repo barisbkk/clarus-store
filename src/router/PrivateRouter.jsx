1700ddb35aef7eacd4f7720468b77bae942a705f
barisbkk <brs2464@gmail.com>
2024-03-07 00:02:07 +0300

    first commit

---
+++ src/router/PrivateRouter.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Navigate, Outlet } from "react-router-dom";
+
+const PrivateRouter = () => {
+  return true ? <Outlet /> : <Navigate to="/" />;
+};
+
+export default PrivateRouter;
+ 

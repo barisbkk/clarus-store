1628c08c219374ae55e0e86997fd35b5c85a2af8
barisbkk <brs2464@gmail.com>
2024-03-07 17:12:55 +0300

    navigation bar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../assets/logo.png";
 import { closeNavbar, openNavbar, logoutIcon } from "../helper/icons";
 import { NavLink } from "react-router-dom";
+
+const navigation = [
+  {
+    title: "Home",
+    path: "/dashboard",
+  },
+  {
+    title: "Products",
+    path: "/dashboard/products",
+  },
+  {
+    title: "About",
+    path: "/dashboard/about",
+  },
+];
+
 const Navbar = () => {
+  const [show, setShow] = useState(false);
+
   return (
     <nav className="bg-navbarColor md:text-sm">
       <div className="gap-x-14 items-center max-w-screen-xl mx-auto px-4 md:flex md:px-8">
@@ -20,29 +38,41 @@ const Navbar = () => {
           </a>
           {/**iconu md ekranlardan sonra gizlensin diyoruz */}
           <div className="md:hidden">
-            <button className="menu-btn text-gray-500 hover:text-gray-800">
-              {closeNavbar} {openNavbar}
+            <button
+              onClick={() => setShow(!show)}
+              className="menu-btn text-gray-500 hover:text-gray-800"
+            >
+              {show ? closeNavbar : openNavbar}
             </button>
           </div>
         </div>
-        <div className="flex flex-col pb-2 flex-1 items-center md:flex md:flex-row">
+        <div
+          className={`${
+            show ? "flex flex-col pb-2" : "hidden"
+          }  flex-1 items-center md:flex md:flex-row`}
+        >
           <ul className="space-y-6 md:flex md:space-x-6 md:space-y-0">
-            <li className="text-gray-700 font-medium  flex justify-center">
-              {/* state true olduğunda linkleri ortalamk için */}
-              <NavLink
-                to="/"
-                className={`block hover:bg-main rounded-full py-2 px-4 hover:text-white`}
+            {navigation.map((item) => (
+              <li
+                key={item.title}
+                className="text-gray-700 font-medium  flex justify-center"
               >
-                "title"
-              </NavLink>
-            </li>
+                {/* state true olduğunda linkleri ortalamak için */}
+                <NavLink
+                  to={item.path}
+                  className={`block hover:bg-main rounded-full py-2 px-4 hover:text-white`}
+                >
+                  {item.title}
+                </NavLink>
+              </li>
+            ))}
           </ul>
           <div className="flex-1 gap-x-6 items-center justify-end mt-6 space-y-6 md:flex md:space-y-0 md:mt-0">
             <NavLink
               to="/"
               className="flex items-center justify-center gap-x-1 py-2 px-4 font-medium text-gray-700 hover:bg-main hover:text-white active:bg-gray-900 rounded-full md:inline-flex"
             >
-              Logout
+              Logout {logoutIcon}
             </NavLink>
           </div>
         </div>

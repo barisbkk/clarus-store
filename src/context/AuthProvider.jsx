32afcdce3dd00475ebfbb79a4f87bc990e5713b9
barisbkk <brs2464@gmail.com>
2024-03-13 12:28:02 +0300

    sessionStorage error fixed

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -7,7 +7,7 @@ export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(
-    JSON.parse(sessionStorage.getItem("usser")) || null
+    JSON.parse(sessionStorage.getItem("user")) || null
   );
   const navigate = useNavigate();
 
@@ -22,7 +22,7 @@ const AuthProvider = ({ children }) => {
 
   useEffect(() => {
     sessionStorage.setItem("user", JSON.stringify(user));
-  }, [user]);
+  }, [user]); //! componentDidUpdate
 
   return (
     <AuthContext.Provider value={{ user, login, logout }}>

afb1446132f62c83f332aaafe4853ebc8db0d1d7
barisbkk <brs2464@gmail.com>
2024-03-13 15:51:39 +0300

    getData and search

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import SearchInput from "../components/SearchInput";
+import ProductCard from "../components/ProductCard";
+import axios, { getAdapter } from "axios";
 
 const Products = () => {
-  return (
-    <div>Products</div>
-  )
-}
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [search, setSearch] = useState("");
+  console.log(search);
+  const getData = async () => {
+    setLoading(true);
+    try {
+      const { data } = await axios(
+        `https://dummyjson.com/products/search?q=${search}`
+      );
+      console.log(data);
+      setProducts(data.products);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-export default Products
+  useEffect(() => {
+    getData();
+  }, [search]); //! search statesi değiştikçe getData fonksiyonu çalışsın
+
+  return (
+    <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+      <SearchInput search={search} setSearch={setSearch} />
+      <h2 className="text-2xl font-bold mt-8 tracking-tight text-gray-900">
+        All Products
+      </h2>
+      <div>
+        <ProductCard />
+      </div>
+    </div>
+  );
+};
+
+export default Products;

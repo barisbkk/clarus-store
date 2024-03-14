d3f169efc0a4c1e2423efb62c3ef35480fd39d96
barisbkk <brs2464@gmail.com>
2024-03-14 16:02:13 +0300

    detail page

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,16 +1,22 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const ProductCard = ({ item }) => {
   const { title, category, thumbnail, price } = item;
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(`/dashboard/products/${title}`, { state: item });
+  };
 
   return (
-    <div className="cursor-pointer">
+    <div className="cursor-pointer" onClick={handleClick}>
       <div className="w-full rounded-md bg-gray-200 hover:opacity-75 lg:h-80">
         <img
           src={thumbnail}
           alt={title}
           title={title} //! title attribute'si element hover olunca bilgi veriyor
-          className="h-[200px] w-full object-fit lg:h-full lg:w-full"
+          className="h-[200px] w-full lg:h-full lg:w-full"
         />
       </div>
       <div className="mt-4 flex justify-between">
